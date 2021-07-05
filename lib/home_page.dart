cb51e1a4167c593a3290c88be77553bb1bdd9790
kazi-rayed-hossain <khossain162030@bscse.uiu.ac.bd>
2021-07-06 03:41:32 +0600

    appbar sidebar

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // variable declaration start
+  final int days = 30;
+  final String name = "dine gayeb";
+
+  // variable declaration end
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("app name"),
+      ),
+        
+      body : Center(
+        child: Container(
+          child: Text('hutu tutu $days $name'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Center (
-          child: Container(
-            child: Text('hutu tutu'),
-
-          ),
-
-        ),
-      ),
-      
+      home: HomePage(),
     );
   }
 }
