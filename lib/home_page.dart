f50a4c9a407f6752a5cab5bcf78bf1508d502339
kazi-rayed-hossain <khossain162030@bscse.uiu.ac.bd>
2021-07-06 03:45:03 +0600

    appbar sidebar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("app name"),
+        title: Text("App name"),
       ),
         
       body : Center(
